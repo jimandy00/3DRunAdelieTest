016cc2fe28a5f7c94d0b5c2d5153ca80865dc593
Jihwan Byeon <wkdghk24@gmail.com>
2023-07-14 11:19:28 +0900

    [create] player move
    [create] player move

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,6 +8,8 @@ using UnityEngine;
 // 사용자를 이동 시키고 싶다.
 public class PlayerMove : MonoBehaviour
 {
+    public float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,24 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * 5 * Time.deltaTime);
+        // Input : 입력 클래스
+        // GetAxix : 메서드
+        // Unity -> Edit -> Setting -> Input
+        // GetAxix 메서드에서 입력한 Stringdmf Input에서 찾아서 가져옴
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        print("h : " + h + ", v : " + v);
+
+        // C = A + B
+        // 좌우와 상하를 더하면 방향(dir)으로 갈 수 있다.
+        // Vector3 dir = Vector3.right * h + Vector3.up * v;
+        Vector3 dir = new Vector3(h, v, 0);
+
+        // 설정된 방향 * 속도 * 시간으로 오브젝트 위치를 이동시킨다.
+        // transform.Translate(dir * speed * Time.deltaTime);
+
+        // p = p0 + vt로 변경
+        transform.position += dir * speed * Time.deltaTime;
     }
 }
